7905f570fff9478d07e2fd5289f5bc3e18f71f7f
Syed israr <121382749+syed1israr@users.noreply.github.com>
2025-06-06 19:11:05 +0530

    Update src/components/Error-state.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/components/Error-state.tsx
+++ src/components/Error-state.tsx
@@ -8,8 +8,8 @@ interface props{
 
 export const Error_state = ({ title, description } : props) => {
     return (
-        <div className="py-4 tx-8 flex flex-1 items-center justify-center">
-            <div className="flex flex-col items-center justify-center gap-y-6 bg-rounded-lg p-10 shadow-sm">
+        <div className="py-4 px-8 flex flex-1 items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-y-6 bg-white rounded-lg p-10 shadow-sm">
                 <AlertCircleIcon className="size-6  text-red-500"/>
                 <div className="flex flex-col gap-y-2 text-center">
                     <h6 className="text-lg font-medium">{title}</h6>
